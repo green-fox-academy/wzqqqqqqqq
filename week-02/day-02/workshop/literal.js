6d0fa12618ca65e3614104410155cc7533dbb565
Zaiquan_wen <wenzaiquan1995@gmail.com>
2019-10-22 17:34:30 +0800

    finished the literal with ES5 and ES6 two versions

---
+++ week-02/day-02/workshop/literal.js
@@ -0,0 +1,41 @@
+function carWrapperES5(model, color, year, doors, specPropName, specPropValue) {
+  // the function should create and return an object with assigning the model,
+  // color year and doors to properties with the same name as the variable and
+  // it should assign to the specPropName named property the specPropValue
+
+  // ES5
+  var car = {
+    model: model,
+    color: color,
+    year: year,
+    doors: doors,
+    specPropName: specPropName,
+    specPropValue: specPropValue
+  }
+
+  return car;
+}
+
+console.log(JSON.stringify(carWrapperES5('Benz', 'black', 1886, 0, 'historical', true)));
+// expected output: { model: 'Benz', color: 'black', year: 1886, doors: 0, historical: true}
+
+function carWrapperES6(model, color, year, doors, specPropName, specPropValue) {
+  // the function should create and return an object with assigning the model,
+  // color year and doors to properties with the same name as the variable and
+  // it should assign to the specPropName named property the specPropValue
+
+  // ES6
+  const car = {
+    model,
+    color,
+    year,
+    doors,
+    specPropName,
+    specPropValue
+  }
+
+  return car;
+}
+
+console.log(JSON.stringify(carWrapperES6('Benz', 'black', 1886, 0, 'historical', true)));
+// expected output: { model: 'Benz', color: 'black', year: 1886, doors: 0, historical: true}
