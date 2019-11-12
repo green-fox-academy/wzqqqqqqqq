6c5e22719f68b6d169cf7dcced60b754c715adea
Zaiquan_wen <wenzaiquan1995@gmail.com>
2019-11-12 15:28:56 +0800

    event registration application finished.

---
+++ week-05/day-02/EventRegisrationApplication/my-app/src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      emailAddress: "",
+      list: []
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { firstName, lastName, emailAddress } = this.state;
+    this.setState({ list: [firstName, lastName, emailAddress] });
+  }
+
+  render() {
+    const { list } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            First name:
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First name "
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+
+          <label>
+            Last name:
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Last name "
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+
+          <label>
+            Email address:
+            <input
+              type="text"
+              name="emailAddress"
+              placeholder="Email address "
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        {list.length === 0 ? null : <h1>Result</h1>}
+        <ul>
+          <li>First name: {list[0]}</li>
+          <li>Last name:{list[1]}</li>
+          <li>Email address:{list[2]}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
