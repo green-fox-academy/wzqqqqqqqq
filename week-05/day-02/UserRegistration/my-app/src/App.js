28ea50ad66ba261346b48f8a98646b66ab8074a3
wzq <wenzaiquan1995@gmail.com>
2019-11-12 23:48:49 +0800

    pizz picker finished.

--- week-05/day-02/UserRegistration/my-app/src/App.js
+++ week-05/day-02/UserRegistration/my-app/src/App.js
@@ -1,93 +1,93 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      userName: "",
-      password: "",
-      email: "",
-      passwordError: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
-
-  validate() {
-    let passwordError = "";
-
-    if (this.state.password.length < 8) {
-      passwordError = "PassWord should be at least 8 characters";
+    constructor() {
+        super();
+        this.state = {
+            userName: "",
+            password: "",
+            email: "",
+            passwordError: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
-    if (passwordError) {
-      this.setState({ passwordError });
-      return false;
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
     }
-    return true;
-  }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    const isValid = this.validate();
-    if (isValid) {
-      console.log(this.state);
-      this.setState({ passwordError: "" });
+    validate() {
+        let passwordError = "";
+
+        if (this.state.password.length < 8) {
+            passwordError = "PassWord should be at least 8 characters";
+        }
+        if (passwordError) {
+            this.setState({passwordError});
+            return false;
+        }
+        return true;
     }
-  }
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Username:
-            <input
-              type="text"
-              name="userName"
-              placeholder="Username "
-              onChange={this.handleChange}
-              required
-            />
-          </label>
-          <div></div>
+    handleSubmit(event) {
+        event.preventDefault();
+        const isValid = this.validate();
+        if (isValid) {
+            console.log(this.state);
+            this.setState({passwordError: ""});
+        }
+    }
 
-          <label>
-            Password:
-            <input
-              type="password"
-              name="password"
-              placeholder="password "
-              onChange={this.handleChange}
-              required
-            />
-          </label>
-          <div style={{ fontSize: 11, color: "red" }}>
-            {this.state.passwordError}
-          </div>
 
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              placeholder="Email "
-              onChange={this.handleChange}
-              required
-            />
-          </label>
-          <div></div>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Username:
+                        <input
+                            type="text"
+                            name="userName"
+                            placeholder="Username "
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </label>
+                    <div></div>
+                    <label>
+                        Password:
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="password "
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </label>
+                    <div style={{fontSize: 11, color: "red"}}>
+                        {this.state.passwordError}
+                    </div>
+
+                    <label>
+                        Email:
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email "
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </label>
+                    <div></div>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
