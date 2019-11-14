26cdd9396f52ea12eba1d9f5d9a1343aaad30a3b
Zaiquan_wen <wenzaiquan1995@gmail.com>
2019-11-14 16:15:17 +0800

    serve first static file.

---
+++ week-05/day-04/Basic/App.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("assets"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+app.listen(port, () => console.log(`Example app listening on port port!`));
