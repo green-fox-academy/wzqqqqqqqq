e4d70ae7baa802281857e316a3557114d360e5c7
Zaiquan_wen <wenzaiquan1995@gmail.com>
2019-11-14 17:27:32 +0800

    Greetings finished.

---
+++ week-05/day-04/Greetings/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// home page
+app.get("/", (req, res) => {
+  // render `home.ejs`
+  let { name } = req.query;
+  let user = name !== undefined ? name : "guest";
+  res.render("home", { title: `Welcome back, ${user}` });
+});
+// app.get("/?name=:name", (req, res) => {
+//   res.render("home", { name: req.query.name || 'Guest' });
+// });
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
