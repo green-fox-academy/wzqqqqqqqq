654954b968939300089ea76a2560898a06059e7a
Zaiquan_wen <wenzaiquan1995@gmail.com>
2019-11-15 15:48:15 +0800

    doubling finished

--- week-05/day-05/Frontend/App.js
+++ week-05/day-05/Frontend/App.js
@@ -1,18 +1,27 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
 const express = require("express");
-const path  = require('path');
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('assets'));
+app.use(express.json());
+app.use(express.static("assets"));
 
-app.get("/", (req, res) =>
- res.sendFile(path.join(__dirname, 'index.html'))
- );
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
- app.listen(PORT, () => {
+app.get("/doubling", (req, res) => {
+  if (req.query.input) {
+    res.send({
+      received: req.query.input,
+      result: req.query.input * 2
+    });
+  } else {
+    res.send({ error: "Please provide an number!" });
+  }
+});
+
+app.listen(PORT, () => {
   console.log(`The server is up and running on ${PORT}`);
 });
- 
